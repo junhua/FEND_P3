05eb6bff072465ae20fb919ecb868f74fe9190f7
Liu Junhua <junhua.viva@gmail.com>
2015-08-22 18:42:35 +0800

    completed

--- js/app.js
+++ js/app.js
@@ -1,35 +1,118 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function(x, y, speed) {
+    // Variables applied to each of our instances go here
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-}
+
+    // if within canvas, move according to speed
+    // elseif move out of canvas, reset 
+    if (this.x <= 505) {
+        this.x += this.speed * dt;
+    } else {
+        this.x = 0;
+    }
+
+    checkCollision(this);
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// check collision
+var checkCollision = function(anEnemy) {
+    // check for collision between enemy and player
+    if (
+        player.y + 131 >= anEnemy.y + 90 &&
+        player.x + 25 <= anEnemy.x + 88 &&
+        player.y + 73 <= anEnemy.y + 135 &&
+        player.x + 76 >= anEnemy.x + 11) {
+
+        player.x = 202.5;
+        player.y = 383;
+    }
+
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player object
+var Player = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-boy.png';
+};
+
+// Draw the player on the screen, required method for game
+// Display score
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.update = function(){}
+
+Player.prototype.handleInput = function(keyPress) {
+    if (keyPress == 'left') {
+        player.x -= player.speed;
+    }
+    if (keyPress == 'up') {
+        player.y -= player.speed - 20;
+    }
+    if (keyPress == 'right') {
+        player.x += player.speed;
+    }
+    if (keyPress == 'down') {
+        player.y += player.speed - 20;
+    }
+
+    checkBoundary(player.x, player.y);
+    checkWin(player.x, player.y);
+};
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+var checkBoundary = function(x, y) {
+    // handle cases where player move to boundaries
+    if (y > 383) {
+        y = 383;
+    }
+    if (x > 402.5) {
+        x = 2.5;
+    }
+    if (x < 2.5) {
+        x = 402.5;
+    }
+}
 
+var checkWin = function(x, y) {
+    // If win Reset player
+    
+    if (y + 63 <= 0) {
+        player.x = 202.5;
+        player.y = 383;
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, 505, 171);
+    }
+};
+
+// Game init
+var allEnemies = [];
+var player = new Player(202.5, 383, 50);
+// let's make it 3 enermies
+
+for (i = 0; i < 3; ++i) {
+    var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
+    allEnemies.push(enemy);
+}
 
 
 // This listens for key presses and sends the keys to your
